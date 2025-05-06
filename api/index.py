d79f3c192e534479c83b6c3c5437b2d951bc3987
André Galdino <andre.gs3@gmail.com>
2025-05-06 14:12:57 -0300

    Update index.py
    fix relative path

--- api/index.py
+++ api/index.py
@@ -12,7 +12,7 @@ CORS(app)
 
 
 # Load the dataset (replace 'your_dataset.csv' with the actual path to your dataset)
-dataset_path = "C:\Users\alexa\OneDrive\Anexos\Fiap\stocksage_fiap_challenge\flask-stocksage-back\dairy\current_processed_data.csv"
+dataset_path = "../dairy/current_processed_data.csv"
 df = pd.read_csv(dataset_path)
 
 
